062d9421760991375deb56eb634f2ef1cd645d56
mozhongzhou <2014160588@qq.com>
2025-03-20 22:26:47 +0800

    更新gitignore 和 demo3/my_call_func_rolling_window_sb3.py 删除不必要的东西

--- demo3/my_call_func_rolling_window_sb3.py
+++ demo3/my_call_func_rolling_window_sb3.py
@@ -1,10 +1,5 @@
 import os
 import numpy as np
-
-# 添加NumPy兼容性补丁
-if not hasattr(np, "NINF"):
-    np.NINF = -np.inf
-    print("已添加NumPy兼容性补丁: np.NINF = -np.inf")
 import pandas as pd
 import matplotlib.pyplot as plt
 from finrl.agents.stablebaselines3.models import DRLAgent
